e98a4eeb416ff3dfe95e8607881b31738492a455
Cehmenang <heaven.cehwin12@gmail.com>
2026-03-28 09:58:12 +0700

    Mau Deploy Frontend Next ke Hostinger

--- app/product/[url]/page.tsx
+++ app/product/[url]/page.tsx
@@ -1,23 +1,25 @@
-"use client"
+"use server"
 
 import { getProductByUrl } from "@/action/product";
 import ProductDetail from "@/components/product/ProductDetail";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Metadata } from "next";
 
-export default function ProductPage() {
-    const { url } = useParams() as { url: string }
-    const [ product, setProduct ] = useState<any>(null)
-    useEffect(()=>{
-        (async function(){
-            const response = await getProductByUrl(url)
-            return setProduct(response.produk)
-        })()
-    }, [])
+export async function generateMetadata({ params }: { params: { url: string } }): Promise<Metadata | any>{
+  try{
+    const { url } = await params
+    const result = await getProductByUrl(url)
+    return {
+      title: result.produk.name,
+      description: result.produk.description,
+      openGraph: { images: [`${process.env.SERVER_API}/storage/${result.produk.images[0][0]}`] }
+    }
+  }catch(err){ console.log(err) }
+}
 
-    useEffect(()=>{
-        console.log(product)
-    }, [product])
+export default async function ProductPage({ params }: { params: { url: string } }) {
+    const { url } = await params
+    const response = await getProductByUrl(url)
+    const product = response.produk
 
     if(!product) return <h1>loading</h1>
 
