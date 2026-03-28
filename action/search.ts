791c9cd929557b6dce5e582a2086d4b16a2e8fbd
Cehmenang <heaven.cehwin12@gmail.com>
2026-03-28 16:38:37 +0700

    Nambah Halaman Login + Testing Update

--- action/search.ts
+++ action/search.ts
@@ -4,7 +4,7 @@ export async function liveSearching(keyword: string){
     try{
         const response = await fetch(`${process.env.SERVER_API}/api/search/${keyword}`, {
             headers: { "Content-Type": "application/json", "Accept": "application/json" },
-            next: { revalidate: 300 }
+            cache: 'no-store'
         })
         const data = await response.json()
         if(response.ok) return { products: data.products, brands: data.brands, categories: data.categories }
