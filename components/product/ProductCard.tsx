b9b187d1d98672311a4f169f1e1db738a4b4ce91
Cehmenang <heaven.cehwin12@gmail.com>
2026-04-13 06:51:32 +0700

    Menambah Banner Schecter Synyster13

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -126,15 +126,16 @@ export default function ProductCard({ product }: { product: IProduct }) {
           <span className="font-bold text-[15px] text-red-500 leading-none">
             {product.offlinePrice && formatPrice(parseInt(product.offlinePrice.includes(" ") ? product.offlinePrice.split(' ')[0].trim() : product.offlinePrice.trim()))}
           </span>
-          {product.pricelist && <div className="font-semibold text-[10px] text-third/50 italic tracking-tighter leading-none">
-            <span>\
+          {product.pricelist && <span className="font-semibold text-[10px] text-third/50 italic tracking-tighter leading-none">
+            <span>
               Pricelist : { formatPrice(parseInt(product.pricelist.includes(" ") ? product.pricelist.split(' ')[0].trim() : product.pricelist.trim()))}
             </span>
             { ((product.pricelist && product.offlinePrice) && ((product.pricelist!.trim() && product.offlinePrice.trim()) 
-              && (parseInt(product.pricelist!) > parseInt(product.offlinePrice)))) && 
-              <span className="text-[11px] bg-amber-600 text-white px-2 py-[2px] rounded-sm font-bold">Disc -{Math.round(((parseInt(product.pricelist!)-parseInt(product.offlinePrice))/parseInt(product.pricelist!))*100)}%</span>
-            }
-          </div>}
+          && (parseInt(product.pricelist!) > parseInt(product.offlinePrice)))) && 
+        <span className="text-[11px] bg-amber-600 text-white px-2 py-[2px] rounded-sm font-bold">Disc -{Math.round(((parseInt(product.pricelist!)-parseInt(product.offlinePrice))/parseInt(product.pricelist!))*100)}%</span>
+      }
+            
+          </span>}
         </div>
           <Link
             href={`/produk/${product.url}`}
