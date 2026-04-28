61e77392264c08e820231d528feb7ddad5e0405f
Cehmenang <heaven.cehwin12@gmail.com>
2026-04-28 10:28:58 +0700

    Menambahkan Opsi Harga5

--- components/product/ProductDetail.tsx
+++ components/product/ProductDetail.tsx
@@ -274,11 +274,11 @@ export default function ProductDetail({ product }: { product?: any }) {
                 <div className="price-section flex gap-x-5">
                     <div suppressHydrationWarning className="font-display leading-none transition-all duration-200 px-5 py-2 border rounded-md border-third">
                       <p className="text-[14px]">Harga Offline</p>
-                      <p className="text-[28px] md:text-[32px] font-black font-family-poppins">{formattedPrice || `Rp ${product.offlinePrice?.toLocaleString()}`}</p>
+                      <p className="text-[28px] md:text-[32px] font-black">{formattedPrice || `Rp ${product.offlinePrice?.toLocaleString()}`}</p>
                     </div>
                     <div suppressHydrationWarning className="font-display leading-none transition-all duration-200 px-5 py-2 border rounded-md border-third">
                       <p className="text-[14px]">Harga Offline</p>
-                      <p className="text-[28px] md:text-[32px] font-black font-family-poppins">{formattedPrice || `Rp ${product.onlinePrice?.toLocaleString()}`}</p>
+                      <p className="text-[28px] md:text-[32px] font-black">{formattedPrice || `Rp ${product.onlinePrice?.toLocaleString()}`}</p>
                     </div>
                 </div>
                 {product.pricelist && <div className="font-semibold text-[16px] text-third/50 italic tracking-tighter leading-none flex gap-x-1 items-center mt-2">
