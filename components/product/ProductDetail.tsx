3f71649e417d0febb9373078ce4e894a6b41144f
Cehmenang <heaven.cehwin12@gmail.com>
2026-04-29 13:27:07 +0700

    Menambahkan Opsi Harga17

--- components/product/ProductDetail.tsx
+++ components/product/ProductDetail.tsx
@@ -341,10 +341,17 @@ export default function ProductDetail({ product }: { product?: any }) {
                     <p className="text-[16px]">Harga Offline</p>
                     <p className="text-[20px] md:text-[28px] font-black text-red-500">{formattedPrice || `Rp ${product.offlinePrice?.toLocaleString()}`}</p>
                   </div>
-                  <div suppressHydrationWarning className="font-display leading-none transition-all duration-200 px-5 py-2 border rounded-md border-third">
+                  {product.onlinePrice.trim() !== "" ? <div suppressHydrationWarning className="font-display leading-none transition-all duration-200 px-5 py-2 border rounded-md border-third">
                     <p className="text-[16px]">Harga Online</p>
                     <p className="text-[20px] md:text-[28px] font-black text-red-500">{formattedPrice || `Rp ${product.onlinePrice?.toLocaleString()}`}</p>
-                  </div>
+                  </div> : ''
+                  
+                  }
+                  <Link href={`https://www.tokopedia.com/bandarmusikjakarta`} suppressHydrationWarning className="font-display leading-none transition-all duration-200 px-5 py-2 border rounded-md border-third">
+                      <p className="text-[16px]">Lihat</p>
+                      <p className="text-[20px] md:text-[28px] font-black">Harga Online</p>
+                    </Link>
+                  
                 </div>
                 {product.pricelist && (
                   <div className="font-semibold text-[20px] text-third/50 italic tracking-tighter leading-none flex gap-x-1 items-center mt-2">
