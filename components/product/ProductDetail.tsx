e67dc0127154562f6ecc841071cb6da73d41750a
Cehmenang <heaven.cehwin12@gmail.com>
2026-05-19 17:22:01 +0700

    Melengkapi Detail Produk13

--- components/product/ProductDetail.tsx
+++ components/product/ProductDetail.tsx
@@ -549,8 +549,8 @@ export default function ProductDetail({ product }: { product?: any }) {
 
               <div className="added-badge flex gap-x-4 mt-16">
 
-                <div className="deliv flex gap-x-2 border-r border-third pr-2">
-                  <Truck size={36} strokeWidth="1"/>
+                <div className="deliv flex gap-x-2 border-r border-third/50 pr-2">
+                  <Truck size={36} strokeWidth="2"/>
                   <div className="deliv-text">
                     <p className="font-bold">Pengiriman Cepat</p>
                     <p className="text-[12px] font-light">Pengiriman barang cepat dan aman</p>
@@ -558,15 +558,15 @@ export default function ProductDetail({ product }: { product?: any }) {
                 </div>
 
                 <div className="guard flex gap-x-2">
-                  <ShieldCheck size={36} strokeWidth="1"/>
+                  <ShieldCheck size={36} strokeWidth="2"/>
                   <div className="guard-text">
                     <p className="font-bold">Garansi 1 Tahun</p>
                     <p className="text-[12px] font-light">Perlindungan sejak tanggal pembelian</p>
                   </div>
                 </div>
 
-                <div className="money flex gap-x-2 border-l border-third pl-2">
-                  <PiggyBank size={36} strokeWidth="1"/>
+                <div className="money flex gap-x-2 border-l border-third/50 pl-2">
+                  <PiggyBank size={36} strokeWidth="2"/>
                   <div className="money-text">
                     <p className="font-bold">Best Price</p>
                     <p className="text-[12px] font-light">Kualitas dengan harga terbaik</p>
