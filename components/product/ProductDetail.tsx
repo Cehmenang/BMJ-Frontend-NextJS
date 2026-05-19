6cc3d88f5a371ed92fe16f9a385c46efe54ba743
Cehmenang <heaven.cehwin12@gmail.com>
2026-05-19 16:47:35 +0700

    Melengkapi Detail Produk3

--- components/product/ProductDetail.tsx
+++ components/product/ProductDetail.tsx
@@ -487,7 +487,7 @@ export default function ProductDetail({ product }: { product?: any }) {
               {variants.length > 0 && <div className="h-px bg-third/8" />}
 
               <div className="att mt-3 mb-1 flex gap-x-3 items-center font-light italic">
-                    <Megaphone size={28} className="p-1 border border-third rounded-full"/>
+                    <Megaphone size={24} className="rounded-full"/>
                     <p className="text-[14px]">Harap Tanya Ketersediaan Barang melalui Whatsapp Sebelum Melakukan Pemesanan.</p>
               </div>
 
