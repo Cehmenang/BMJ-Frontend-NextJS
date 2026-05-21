f493491317e52b1c0594d31ec53e73f26c06f6fa
Cehmenang <heaven.cehwin12@gmail.com>
2026-05-21 12:38:20 +0700

    Ongkirrrr18

--- components/product/CekOngkir.tsx
+++ components/product/CekOngkir.tsx
@@ -6,18 +6,14 @@ import { useState } from "react"
 export default function cekOngkir(){
     const [destInput, setDestInput] = useState<string>('')
 
-    async function ambilDest(destInput: string){
-        const data = await getDestinationOption(destInput!)
-        console.log(data)
-    }
-
     return (
         <div className="bg-ongkir-overlay fixed flex justify-center items-center top-0 left-0 w-[100%] h-dvh bg-third/50">
             <div className="form-ongkir bg-primary">
                 <input type="text" name="destination" placeholder="Kota/Kecamatan Tujuan" onChange={(e)=>setDestInput(e.target.value)}/>
-                <button onClick={()=>{ 
+                <button onClick={async()=>{ 
                     if(destInput && destInput.trim() !== ""){
-                        ambilDest(destInput)
+                        const data = await getDestinationOption(destInput!)
+                        console.log(data, 'DATAAA')
                     }
                 }}>Cari Lokasi</button>
             </div>
