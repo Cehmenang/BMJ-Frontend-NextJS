0874c36b7ccfcb3dd3c5a884f8f75b44106cc523
Cehmenang <heaven.cehwin12@gmail.com>
2026-05-21 07:31:06 +0700

    Melengkapi Detail Produk65

--- app/api/ongkir/route.ts
+++ app/api/ongkir/route.ts
@@ -1,13 +1,13 @@
-"use server"
-
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest){
     const searchParams = request.nextUrl.searchParams
     const location = searchParams.get('location')
-    const response = await fetch(`https://rajaongkir.komerce.id/api/v1/destination/domestic-destination?search=${location}&limit=999&offset=999`, {
+    const response = await fetch(`https://rajaongkir.komerce.id/api/v1/destination/domestic-destination?search=${location}`, {
         method: 'GET',
         headers: { 'key': process.env.SHIPPING! }
     }) as any
-    if(response.ok){ return response.data }
+    if(response.ok){ return NextResponse.json({
+        data: await response.json()
+    }) }
 }
