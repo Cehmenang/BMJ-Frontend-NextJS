4376670749d59977a9a65e8e3300c2c3a7add88f
Cehmenang <heaven.cehwin12@gmail.com>
2026-05-21 11:06:28 +0700

    Ongkirrr

--- components/product/CekOngkir.tsx
+++ components/product/CekOngkir.tsx
@@ -4,24 +4,22 @@ import { getDestinationOption } from "@/action/product";
 import { useState } from "react";
 
 export default function cekOngkir(){
-    const [destInput, setDestInput] = useState<string>()
+
+    async function handleDestinationOption(e: any){
+        e.preventDefault()
+        const form = new FormData(e.currentTarget)
+        const location = form.get('destination') as string
+        const data = await getDestinationOption(location)
+        console.log(data)
+    }
 
     return (
         <div className="bg-ongkir-overlay fixed flex justify-center items-center top-0 left-0 w-[100%] h-dvh bg-third/50">
             <div className="form-ongkir bg-primary">
-                <input type="text" name="destination" placeholder="Kota/Kecamatan Tujuan" onChange={(e)=>setDestInput(e.target.value)}/>
-                <button onClick={async()=>{
-                    if(destInput && destInput.trim()){
-                       try{
-                            const response = await fetch('/api/ongkir?location=sawah+besar', { method: 'GET' })
-                            const data = await response.json()
-                            console.log(data)
-                            console.log('KITAAA COBAINNNN')
-                            console.log(data, 'DATANYEEEE')
-                            if(response.ok){ console.log('BERHASILLL') }
-                        }catch(err){ console.log(err) }
-                    } 
-                }}>Cari Lokasi</button>
+                <form onSubmit={handleDestinationOption}>
+                    <input type="text" name="destination" placeholder="Kota/Kecamatan Tujuan"/>
+                    <button type="submit">Cari Lokasi</button>
+                </form>
             </div>
         </div>
     )
