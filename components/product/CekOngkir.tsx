35bad3176efea3e75d5c3dbf6b84fd8e335a5543
Cehmenang <heaven.cehwin12@gmail.com>
2026-05-21 10:31:21 +0700

    Melengkapi Detail Produk88

--- components/product/CekOngkir.tsx
+++ components/product/CekOngkir.tsx
@@ -7,8 +7,8 @@ export default function cekOngkir(){
     const [destInput, setDestInput] = useState<string>()
 
     return (
-        <div className="bg-ongkir-overlay absolute top-0 left-0 w-[100%] h-dvh bg-third/50">
-            <div className="form-ongkir">
+        <div className="bg-ongkir-overlay fixed flex justify-center items-center top-0 left-0 w-[100%] h-dvh bg-third/50">
+            <div className="form-ongkir bg-primary">
                 <input type="text" name="destination" placeholder="Kota/Kecamatan Tujuan" onChange={(e)=>setDestInput(e.target.value)}/>
                 <button onClick={async()=>{
                     if(destInput && destInput?.trim()){
