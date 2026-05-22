185bc0bdd84082ea999d9b65ec690d6761fa770b
Cehmenang <heaven.cehwin12@gmail.com>
2026-05-22 12:40:41 +0700

    Kategori9

--- app/kategori/page.tsx
+++ app/kategori/page.tsx
@@ -1,4 +1,14 @@
+import { getCategories } from "@/action/kategori"
+import { useEffect } from "react"
+
 export default function Kategori(){
+  useEffect(()=>{
+    (async function(){
+      const data = await getCategories()
+      console.log(data)
+    })()
+  }, [])
+
   return (
     <div className="kategori">
       <h1>Kategori Page</h1>
