16f26da566014b3b2a7943ef74ca2c1cfdcd1931
Cehmenang <heaven.cehwin12@gmail.com>
2026-06-04 11:48:34 +0700

    layout6

--- components/product/ProductDetail.tsx
+++ components/product/ProductDetail.tsx
@@ -575,7 +575,7 @@ export default function ProductDetail({ product }: { product?: any }) {
                     <span>Keranjang</span>
                   </button>
 
-                  <Link href={'https://www.tokopedia.com/bandarmusikjakarta'} className="p-2 border-2 border-third rounded-md">
+                  <Link href={'https://www.tokopedia.com/bandarmusikjakarta'} className="p-1 border-2 border-third rounded-md">
                       <Image src={'/tokped.webp'} alt="Tokopedia - Bandar Musik Jakarta" width={30}/>
                   </Link>
 
