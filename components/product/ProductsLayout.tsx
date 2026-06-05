cf20c61531e66a85ae450cfbe98873635cccfff1
Cehmenang <heaven.cehwin12@gmail.com>
2026-06-05 17:15:06 +0700

    Memperbaiki W65

--- components/product/ProductsLayout.tsx
+++ components/product/ProductsLayout.tsx
@@ -353,8 +353,8 @@ export default function ProductsLayout({
                 <path d="M21 12a9 9 0 1 1-6.219-8.56" />
               </svg>
             )}
-            <span className="font-bold flex gap-x-2">
-                <span className="text-amber-600 bg-amber-200 border-2 border-amber-400 rounded-md px-2 py-1">{totalProducts}</span>
+            <span className="font-bold flex gap-x-2 items-center justify-center">
+                <span className="text-amber-600 bg-amber-200 border-2 border-amber-400 rounded-sm px-2 py-[2px]">{totalProducts}</span>
                 <span className="italic text-third/50">Produk</span>
               </span>
             {searchParams.get("q") && (
