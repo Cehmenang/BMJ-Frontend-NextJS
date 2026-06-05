734a26e66acf231f393abc32b93c0ec569b861da
Cehmenang <heaven.cehwin12@gmail.com>
2026-06-05 16:57:24 +0700

    Memperbaiki WA3

--- components/product/ProductDetail.tsx
+++ components/product/ProductDetail.tsx
@@ -216,8 +216,7 @@ export default function ProductDetail({ product }: { product?: any }) {
   useEffect(() => {
     if (product) {
       const waMessage = encodeURIComponent(
-        `Halo, saya ingin menanyakan ketersediaan :\nNama Produk: ${product?.name}\nHarga Offline: ${formatPrice(product.offlinePrice)}\nTokopedia: ${product.tautan ? product.tautan : 'https://www.tokopedia.com/bandarmusikjakarta'}\n\n
-        Apakah barangnya tersedia dan harganya sesuai?\n\nUntuk detailnya ada disini https://bandarmusikjakarta.com/produk/${product.url}. Terimakasih`
+        `Halo, saya ingin menanyakan ketersediaan :\nNama Produk: ${product?.name}\nHarga Offline: ${formatPrice(product.offlinePrice)}\nTokopedia: ${product.tautan ? product.tautan : 'https://www.tokopedia.com/bandarmusikjakarta'}\n\nApakah barangnya tersedia dan harganya sesuai?\nUntuk detailnya ada disini https://bandarmusikjakarta.com/produk/${product.url}. Terimakasih`
       );
       setWaUrl(`https://wa.me/6281929290560?text=${waMessage}`);
     }
