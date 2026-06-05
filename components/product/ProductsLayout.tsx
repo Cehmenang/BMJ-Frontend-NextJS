cf21b6f58f48ae77afea1bc3b35b3be5c0b26710
Cehmenang <heaven.cehwin12@gmail.com>
2026-06-05 17:09:22 +0700

    Memperbaiki WA4

--- components/product/ProductsLayout.tsx
+++ components/product/ProductsLayout.tsx
@@ -353,7 +353,10 @@ export default function ProductsLayout({
                 <path d="M21 12a9 9 0 1 1-6.219-8.56" />
               </svg>
             )}
-            <span className="font-bold text-third">{totalProducts} produk</span>
+            <span className="font-bold">
+                <span className="text-amber-700 bg-amber-200 border border-amber-300 rounded-xl px-2 py-1">{totalProducts}</span>
+                <span className="italic text-third/50">Produk</span>
+              </span>
             {searchParams.get("q") && (
               <> · "<span className="text-third/55">{searchParams.get("q")}</span>"</>
             )}
