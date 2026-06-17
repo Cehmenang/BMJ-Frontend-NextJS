6f5753781073117dd0fda8658fcf1773593e8ea1
Cehmenang <heaven.cehwin12@gmail.com>
2026-06-17 13:47:11 +0700

    Not Foumd2

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 export default function NotFound(){
     return (
-        <div className="not-found flex justify-center items-center flex-col">
+        <div className="not-found h-dvh flex justify-center items-center flex-col">
             <h1 className="text-[40px]">Halaman Tidak Ditemukan</h1>
-            <p className="opacity-50 group">Kembali ke <Link href={'/'} className="group-hover:opacity-100 transition">halaman utama.</Link></p>
+            <div>
+                <p className="opacity-50">Kembali ke </p>
+                <Link href={'/'} className="group-hover:opacity-100 transition underline">halaman utama.</Link></div>
         </div>
     )
 }
