cb06a2eb7ecc5f1e7339caff37165dbd77c69731
Cehmenang <heaven.cehwin12@gmail.com>
2026-06-23 18:59:41 +0700

    menambahkan promo8

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -164,8 +164,9 @@ export default function ProductCard({
 
         {/* Promo */}
         {product.promo && <div className="product-promo absolute top-20 left-0">
-            <h1>{product.namaPromo}</h1>
-            <h1>{formatPrice(parseInt(product.promo))}</h1>
+          <h1>HAITAYO</h1>
+            {/* <h1>{product.namaPromo}</h1> */}
+            {/* <h1>{formatPrice(parseInt(product.promo))}</h1> */}
         </div>}
 
         {/* Toast */}
