26485e41f9d4009c040ccd0c304bffaa15b63d5e
Cehmenang <heaven.cehwin12@gmail.com>
2026-07-01 10:57:25 +0700

    MERAPIKAN LAYOUT5

--- app/kategori/[title]/page.tsx
+++ app/kategori/[title]/page.tsx
@@ -3,6 +3,7 @@
 import { getProductByCategory } from "@/action/product"
 import ProductDetail from "@/components/product/ProductDetail"
 import ProductsLayout from "@/components/product/ProductsLayout"
+import { IProduct } from "@/interface"
 
 export default async function CategoryDetail({ params, searchParams }: { 
     params: { title: string },
@@ -18,6 +19,8 @@ export default async function CategoryDetail({ params, searchParams }: {
     const { page, q, sort, kategori, stock } = await searchParams
     const pageValue = Number(page) || 1
     const result = await getProductByCategory(title, pageValue)
+    const brands = result.data.map((produk: IProduct)=>produk.brandId)
+    console.log(brands, 'BRAND')
 
     return (
         <div className="main-category">
