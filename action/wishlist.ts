603a46372fdee4b0cbb9b4f46c9b4c0e8c7ac470
Cehmenang <heaven.cehwin12@gmail.com>
2026-07-07 10:43:59 +0700

    MERAPIKAN LAYOUT72

--- action/wishlist.ts
+++ action/wishlist.ts
@@ -24,8 +24,11 @@ export async function getWishlists(token: string){
 
 export async function removeWishlist(id: string){
     try{
-        const response = await fetch(`${process.env.SERVER_API}/api/wishlist/hapus/${id}`, { method: 'GET', headers: { "Accept": "application/json" } })
-        console.log(response)
+        const cookieStore = await cookies()
+        const response = await fetch(`${process.env.SERVER_API}/api/wishlist/hapus/${id}`, { method: 'GET', 
+            headers: { 
+                "Accept": "application/json", Authorization: `Bearer ${cookieStore.get('access_token')!.value}`
+            } })
         if(response.ok) return true
     }catch(err){ console.log(err) }
 }
