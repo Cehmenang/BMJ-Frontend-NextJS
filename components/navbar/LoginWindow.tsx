f238c08745347ecfefc4b20b91af83c8d3e63242
Cehmenang <heaven.cehwin12@gmail.com>
2026-08-27 11:27:33 +0700

    LOGIN WINDOW2

--- components/navbar/LoginWindow.tsx
+++ components/navbar/LoginWindow.tsx
@@ -1,10 +1,9 @@
 import { SetStateAction } from "react";
-import { FaLess } from "react-icons/fa6";
 
 export default function LoginWindow({ setNeedLogin }: { setNeedLogin: SetStateAction<any> }){
     return (
-        <div className="login-window absolute z-50 w-full h-dvh bg-third/70">
-            <button onClick={()=>setNeedLogin(FaLess)}>KLOS</button>
+        <div className="login-window absolute z-[120] w-full h-dvh bg-third/70">
+            <button onClick={()=>setNeedLogin(false)}>KLOS</button>
             <h1 className="text-[100px] text-primary">LOGIN WINDOW!</h1>
         </div>
     )
