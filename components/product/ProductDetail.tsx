1a658184859f1a6cff4e4b6028185b106e0528fc
Cehmenang <heaven.cehwin12@gmail.com>
2026-08-27 17:56:13 +0700

    LOGIN WINDOW29

--- components/product/ProductDetail.tsx
+++ components/product/ProductDetail.tsx
@@ -747,7 +747,7 @@ const handleCloseToast = () => {
                   </div>
                   <button
                     disabled={!allVariantsSelected || product.stock == 0}
-                    className="flex-1 py-2.5 px-4 bg-second text-third border-2 border-third rounded-md font-bold text-[16px] md:text-[18px] flex items-center justify-center gap-2 transition-all duration-150 disabled:bg-slate-800 disabled:text-primary disabled:cursor-not-allowed drop-shadow-[4px_4px_0px_rgba(62,63,32,1)] md:drop-shadow-[6px_6px_0px_rgba(62,63,32,1)]"
+                    className="flex-1 py-2.5 px-4 bg-second text-third border-2 border-third rounded-md font-bold text-[16px] md:text-[18px] flex items-center justify-center gap-2 transition-all duration-150 disabled:bg-slate-800 disabled:text-slate-300 disabled:cursor-not-allowed drop-shadow-[4px_4px_0px_rgba(62,63,32,1)] md:drop-shadow-[6px_6px_0px_rgba(62,63,32,1)]"
                     onClick={async () =>{ 
                       try {
                         await createWishlist(product.id, qty);
