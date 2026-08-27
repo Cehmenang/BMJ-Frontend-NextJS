b1aefbf88521d1ab9aacd0a0919bcea2108399bd
Cehmenang <heaven.cehwin12@gmail.com>
2026-08-27 17:52:22 +0700

    LOGIN WINDOW26

--- components/product/ProductDetail.tsx
+++ components/product/ProductDetail.tsx
@@ -747,7 +747,7 @@ const handleCloseToast = () => {
                   </div>
                   <button
                     disabled={!allVariantsSelected || product.stock == 0}
-                    className="flex-1 py-2.5 px-4 bg-second text-third border-2 border-third rounded-md font-bold text-[16px] md:text-[18px] flex items-center justify-center gap-2 transition-all duration-150 disabled:opacity-40 disabled:cursor-not-allowed drop-shadow-[4px_4px_0px_rgba(62,63,32,1)] md:drop-shadow-[6px_6px_0px_rgba(62,63,32,1)]"
+                    className="flex-1 py-2.5 px-4 bg-second text-third border-2 border-third rounded-md font-bold text-[16px] md:text-[18px] flex items-center justify-center gap-2 transition-all duration-150 disabled:bg-second/60 disabled:cursor-not-allowed drop-shadow-[4px_4px_0px_rgba(62,63,32,1)] md:drop-shadow-[6px_6px_0px_rgba(62,63,32,1)]"
                     onClick={async () =>{ 
                       try {
                         await createWishlist(product.id, qty);
@@ -760,7 +760,7 @@ const handleCloseToast = () => {
                     }}
                   >
                     <ShoppingCart strokeWidth={3} className="w-5 h-5 md:w-6 md:h-6" />
-                    <span>Keranjang</span>
+                    <span>{product.stock == 0 ? "Pre-Order" : "Keranjang"}</span>
                   </button>
                   <Link href={`${product.tautan || "https://www.tokopedia.com/bandarmusikjakarta"}`}
                     target="_blank"
@@ -787,10 +787,10 @@ const handleCloseToast = () => {
                   </Link>
                 )}
 
-                <div className="flex gap-x-2 items-start text-third/50 mt-1">
+                <div className="flex gap-x-2 items-start text-third mt-1">
                   <Megaphone size={16} className="flex-shrink-0 mt-0.5" />
-                  <p className="text-[11px] md:text-[12px] font-light italic leading-snug">
-                    Harap Tanya Ketersediaan Barang melalui Whatsapp Before Melakukan Pemesanan.
+                  <p className="text-[11px] md:text-[12px] italic leading-snug">
+                    Harap Tanya Ketersediaan Barang Melalui Whatsapp Sebelum Melakukan Pemesanan.
                   </p>
                 </div>
               </div>
